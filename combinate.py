ab999b4db65f74ee21fb482adbb58cad75d1798a
Toanil <congtoan.dn@gmail.com>
2022-05-31 05:36:28 +0700

    update dataset to 4k4 samples and do some eda

--- combinate.py
+++ combinate.py
@@ -1,15 +1,15 @@
 import numpy as np
 import pandas as pd
 
-df1 = pd.read_csv("./mycsvfile.csv", encoding="utf-8")
-df2 = pd.read_csv("./mycsvfile3.csv", encoding="utf-8")
-df3 = pd.read_csv("./mycsvfile4.csv", encoding="utf-8")
-df4 = pd.read_csv("./mycsvfile5.csv", encoding="utf-8")
-df5 = pd.read_csv("./mycsvfile6.csv", encoding="utf-8")
-df6 = pd.read_csv("./mycsvfile7.csv", encoding="utf-8")
+df1 = pd.read_csv("./data-5k/mycsvfile.csv", encoding="utf-8")
+df2 = pd.read_csv("./data-5k/mycsvfile5k.csv", encoding="utf-8")
+df3 = pd.read_csv("./data-5k/mycsvfile5k-1.csv", encoding="utf-8")
+df4 = pd.read_csv("./data-5k/mycsvfile5k-2.csv", encoding="utf-8")
+df5 = pd.read_csv("./data-5k/mycsvfile5k-3.csv", encoding="utf-8")
+df6 = pd.read_csv("./data-5k/mycsvfile5k-4.csv", encoding="utf-8")
 
 frames = [df1, df2, df3, df4, df5, df6]
 result = pd.concat(frames)
-result.to_csv("review_film_reelgood.csv", encoding="utf-8", index=False)
+result.to_csv("film_reelgood_5k.csv", encoding="utf-8", index=False)
 print(result.info())
 
--- test.py
+++ test.py
@@ -1,6 +1,23 @@
 import pandas as pd
 import numpy as np
 
-df_film = pd.read_csv("review_film_reelgood_full.csv", encoding="utf-8")
+df_film = pd.read_csv("film_reelgood_5k.csv", encoding="utf-8")
 print(df_film.info())
 print(df_film.head(5))
+
+arr_votes = []
+
+for i in df_film['Description']:
+    sub_str = i[-50:]
+    startIndex = sub_str.find("(")
+    endIndex = sub_str.find(")")
+    if('votes' in sub_str[startIndex: endIndex]):
+        arr_votes.append(sub_str[startIndex + 1: endIndex])
+    else: arr_votes.append(np.NaN)
+
+arr_votes = np.array(arr_votes)
+df_votes = pd.DataFrame(arr_votes, columns = ['User_Votes'])
+result = pd.concat([df_film, df_votes], axis=1)
+result.info()
+result.head(2)
+result.to_csv("film_full.csv", encoding="utf-8")
